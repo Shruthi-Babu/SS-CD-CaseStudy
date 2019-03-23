f32bacafad387253489c9c6ae445cae65b849a67
Shruthi-Babu <shruthib724@gmail.com>
2019-03-23 23:03:23 +0530

    Program to analyse added

---
+++ question.c
@@ -0,0 +1,9 @@
+#include <stdio.h>	//to include header files
+int main()	//start point of the program
+{
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   int a=2, b=3;	/* initialise two numbers
+with two values */
+   return a+b;	//final answer
+}
